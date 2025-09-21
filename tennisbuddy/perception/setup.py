1539c7c00b0c430f9a8d3ddc56e55fd80697d0e7
hectorrrrr <zhilinhe@usc.edu>
2025-09-21 19:01:02 -0400

    perception package build && fix

--- tennisbuddy/perception/setup.py
+++ tennisbuddy/perception/setup.py
@@ -1,26 +1,35 @@
 from setuptools import setup, find_packages
-package_name = 'tennisbuddy_perception'
+from pathlib import Path
+
+ros_pkg_name = 'tennisbuddy_perception'
+py_pkg_root = 'src'
+py_pkg_name = 'perception'
+
+Path('resource').mkdir(exist_ok=True)
+Path(f'resource/{ros_pkg_name}').touch(exist_ok=True)
 
 setup(
-    name=package_name,
-    version='0.2.0',
-    packages=find_packages(),
+    name=ros_pkg_name,
+    version='0.1.0',
+    packages=find_packages(where=py_pkg_root, include=[py_pkg_name, py_pkg_name + '.*']),
+    package_dir={'': py_pkg_root},
     data_files=[
-        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
-        ('share/' + package_name + '/launch', [
-            'launch/perception_gazebo_gt.launch.py',
+        ('share/ament_index/resource_index/packages', [f'resource/{ros_pkg_name}']),
+        ('share/' + ros_pkg_name + '/launch', [
             'launch/ball_spawner.launch.py',
+            'launch/perception_gazebo_gt.launch.py',
         ]),
-        ('share/' + package_name, ['package.xml']),
+        ('share/' + ros_pkg_name + '/configs', []),
+        ('share/' + ros_pkg_name, ['package.xml']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    description='Perception helpers for ros_gz: ball spawner & groundtruth',
     license='Apache-2.0',
     entry_points={
         'console_scripts': [
-            'ball_spawner = tennisbuddy_perception.nodes.ball_spawner:main',
-            'ball_groundtruth = tennisbuddy_perception.nodes.ball_groundtruth:main',
+            'ball_spawner = perception.nodes.ball_spawner:main',
+            'ball_groundtruth = perception.nodes.ball_groundtruth:main',
+            'mock_spawn_service = perception.nodes.mock_spawn_service:main'
         ],
     },
 )
--- tennisbuddy/planning/setup.py
+++ tennisbuddy/planning/setup.py
@@ -1,44 +1,37 @@
 from setuptools import setup, find_packages
-import os
-from glob import glob
+from pathlib import Path
 
-package_name = 'tennisbuddy_planning'
+ros_pkg_name = 'tennisbuddy_planning'
+py_pkg_root = 'src'
+py_pkg_name = 'planning'
 
-# Install launch and config files into share/<pkg>/
-launch_files = glob(os.path.join('launch', '*.launch.py')) + [
-    os.path.join('launch', 'navigation_launch.py'),
-    os.path.join('launch', 'nav2_backend.py'),
-]
-config_files = glob(os.path.join('config', '*.yaml'))
-
-data_files = [
-    ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
-    ('share/' + package_name, ['package.xml']),
-    ('share/' + package_name + '/launch', launch_files),
-    ('share/' + package_name + '/config', config_files),
-]
+Path('resource').mkdir(exist_ok=True)
+Path(f'resource/{ros_pkg_name}').touch(exist_ok=True)
 
 setup(
-    name=package_name,
+    name=ros_pkg_name,
     version='0.1.0',
-    packages=find_packages(exclude=['test']),
-    data_files=data_files,
+    packages=find_packages(where=py_pkg_root, include=[py_pkg_name, py_pkg_name + '.*']),
+    package_dir={'': py_pkg_root},
+    data_files=[
+        ('share/ament_index/resource_index/packages', [f'resource/{ros_pkg_name}']),
+        ('share/' + ros_pkg_name + '/launch', [
+            'launch/planning_tennisbuddy.launch.py',
+            'launch/planning_nav2_path.launch.py',
+            'launch/planning_nav2_goal.launch.py',
+        ]),
+        ('share/' + ros_pkg_name + '/configs', []),
+        ('share/' + ros_pkg_name, ['package.xml']),
+    ],
     install_requires=['setuptools'],
     zip_safe=True,
-    description='Planning stack for TennisBuddy: planner/tracker and Nav2 bridges.',
     license='Apache-2.0',
-    tests_require=['pytest'],
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: Apache Software License'
-    ],
     entry_points={
         'console_scripts': [
-            # nodes/
-            'planner_node = tennisbuddy_planning.nodes.planner_node:main',
-            'trajectory_tracker = tennisbuddy_planning.nodes.trajectory_tracker:main',
-            'nav2_goal_pusher = tennisbuddy_planning.nodes.nav2_goal_pusher:main',
-            'nav2_path_bridge = tennisbuddy_planning.nodes.nav2_path_bridge:main',
+            'planner_node = planning.nodes.planner_node:main',
+            'trajectory_tracker = planning.nodes.trajectory_tracker:main',
+            'nav2_goal_pusher = planning.nodes.nav2_goal_pusher:main',
+            'nav2_path_bridge = planning.nodes.nav2_path_bridge:main',
         ],
     },
 )
