69d9903e509c3423b196c254b4948d29c8d8b9e1
hectorrrrr <zhilinhe@usc.edu>
2025-11-04 17:13:48 -0500

    cv nodes

--- tennisbuddy/perception/setup.py
+++ tennisbuddy/perception/setup.py
@@ -31,7 +31,7 @@ setup(
             'ball_groundtruth = perception.nodes.ball_groundtruth:main',
             'mock_spawn_service = perception.nodes.mock_spawn_service:main',
             'ball_detection_bridge = perception.nodes.ball_detection_bridge:main',
-            'detector_receiver = perception.nodes.detector_receiver:main',
+            'detect_receiver = perception.nodes.detect_receiver:main',
             'yolo_realsense_detector = perception.nodes.yolo_realsense_detector:main',
         ],
     },
